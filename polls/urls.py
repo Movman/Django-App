5ac53847e925c760e288bfa84cfd3728e318f561
Gabriel Faltin <gabifaltin@seznam.cz>
2021-11-08 13:52:05 +0100

    Comment added at line 8

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
+    # as_view() - preddefinovana funkcia?
     path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
