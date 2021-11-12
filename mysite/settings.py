7f1b263d8f6d210d3be00f895bb92a666c147bdd
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2021-11-12 09:53:10 +0100

    Modified settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls.apps.PollsConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
