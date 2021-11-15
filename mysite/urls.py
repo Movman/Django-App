c2b9b2b5b9aa7dc6e24b46d6997310a286c98b6d
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2021-11-15 11:32:11 +0100

    modified url for api

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('api/', include('polls.api_urls')),
+    path('api/polls/', include('polls.api_urls')),
 ]
