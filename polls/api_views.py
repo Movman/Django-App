bfb25d42f033a3ccb3b407e28044fa06d0dfffaa
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2021-11-15 10:28:23 +0100

    api_views file added

---
+++ polls/api_views.py
@@ -0,0 +1,25 @@
+from rest_framework.decorators import api_view
+from .serializers import PollsSerializer, ChoiceSerializer, PollsDetailSerializer
+from rest_framework.response import Response
+
+from .models import Question, Choice
+
+# API views
+# Making my own view ---Practice---
+
+@api_view(['GET'])
+def pollsList(request):
+    question = Question.objects.all()
+    serializer = PollsSerializer(question, many=True)
+    
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def pollsDetail(request, pk):
+    poll = Question.objects.get(id=pk)
+    choice = Choice.objects.all()
+    # serializer = ChoiceSerializer(choice, many=True)
+    serializer = PollsDetailSerializer(poll)
+
+    return Response(serializer.data)
