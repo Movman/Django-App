a88f476ce64186bc4efeb0fdff7a037f698aab76
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2021-11-15 09:43:22 +0100

    removed api urls

--- polls/urls.py
+++ polls/urls.py
@@ -12,8 +12,4 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    # /polls-list/
-    path('polls-list/', views.pollsList, name="polls-list"),
-    # /polls-detail/1/
-    path('polls-detail/<int:pk>/', views.pollsDetail, name="polls-detail"),
 ]
