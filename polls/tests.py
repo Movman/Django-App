2fa98347a2e14f1072bde15f81f2b11d22ceb024
Michael Niepel <michael.niepel@exxeta.com>
2021-11-24 10:51:32 +0100

    fix vote API

--- polls/tests.py
+++ polls/tests.py
@@ -187,11 +187,51 @@ class PollsTestCase(APITestCase):
 
     def test_non_exist_polls_vote(self):
         # empty DB - error expected
-        response = self.client.get("api/polls/votes/1/")
+        response = self.client.post("/api/polls/votes/1/", {})
+        # mal by to byt JSON 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_can_vote_poll(self):
-    #     question = Question.objects.create({'question_text': 'test'})
-    #     choice = Choice.objects.create(question, {'choice_text': 'test'})
-    #     response = self.client.post("api/polls/votes/", choice)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_can_vote_poll(self):
+        # setup
+        question = create_question_with_choices('Aky je den?', 'Piatok', 'Pondelok')
+        choice = question.choices.first()
+        self.assertEqual(question.choices.count(), 2)
+        self.assertEqual(choice.votes, 0)
+
+        url = reverse("polls-votes", args=(question.id,))
+
+        self.assertEqual(url, "/api/polls/1/vote/")
+        # akciu
+        response = self.client.post(url, {'choice': choice.id})
+
+        # TEST :)
+        choice.refresh_from_db()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(choice.votes, 1)
+
+    def test_cannot_vote_poll_with_nonexisting_choice(self):
+        # setup
+        question = create_question_with_choices('Aky je den?', 'Piatok', 'Pondelok')
+
+        url = reverse("polls-votes", args=(question.id,))
+
+        self.assertEqual(url, "/api/polls/1/vote/")
+        # akciu
+        response = self.client.post(url, {'choice': 238746})
+
+        # TEST :)
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_cannot_vote_poll_with_no_data_sent(self):
+        # setup
+        question = create_question_with_choices('Aky je den?', 'Piatok', 'Pondelok')
+
+        url = reverse("polls-votes", args=(question.id,))
+
+        self.assertEqual(url, "/api/polls/1/vote/")
+        # akciu
+        response = self.client.post(url)
+
+        # TEST :)
+        self.assertEqual(response.status_code, 400)
