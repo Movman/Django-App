6e6581c5b9d8648c0a04d9f97f4c75c74e1c5ea6
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2021-12-13 09:57:41 +0100

    edited BlogPage model, new ContactPage model

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.db.models.fields.files import ImageField
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
 
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.search import index
+from wagtail.images.blocks import ImageChooserBlock
 
 # Create your models here.
 
@@ -18,10 +21,27 @@ class IndexPage(Page):
 
 
 class BlogPage(Page):
-    body = RichTextField()
+    image = ImageField(blank=True, default=None)
+    author = models.CharField(max_length=200, default=None)
+    date = models.DateField("Post date", default=None)
+    text = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], default=None)
 
     # Editor panels configuration
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('image'),
+        FieldPanel('author'),
+        FieldPanel('date'),
+        StreamFieldPanel('text'),
+    ]
+
+class ContactPage(Page):
+    message = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("message")
     ]
