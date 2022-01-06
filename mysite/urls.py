2cf902a2890a09d69b31d3f594db8cd89ab27454
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2022-01-06 17:22:13 +0100

    #23 Added URL for allauth registration/login

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,18 +1,3 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 
@@ -24,7 +9,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from pages.views import search, ContactFormView
 
-
 # /kontakt
 # /admin
 # /polls
@@ -35,6 +19,7 @@ urlpatterns = [
     path('api/polls/', include('polls.api_urls')),
     # wagtail urls
     path('admin/', include(wagtailadmin_urls)),
+    path('accounts/', include('allauth.urls')),
     # path('documents/', include(wagtaildocs_urls)),
     # default na konci
     path('search/', search, name='search'),
