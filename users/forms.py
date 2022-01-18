833699fe617fb834f8c907b0153e9b37a934e38a
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2022-01-18 08:37:23 +0100

    edit

--- users/forms.py
+++ users/forms.py
@@ -4,10 +4,15 @@ from django.utils.translation import gettext_lazy as _
 from wagtail.users.forms import UserEditForm, UserCreationForm
 
 from users.models import AuthorProfile
+from polls.models import Question, Choice
 
 class CustomUserEditForm(UserEditForm):
     profile = forms.ModelChoiceField(queryset=AuthorProfile.objects, required=False, label=_("Profile"))
+    polls = forms.ModelChoiceField(queryset=Question.objects, required=False, label=_("Question"))
+    choice = forms.ModelChoiceField(queryset=Choice.objects, required=False, label=_("Choice"))
 
 
 class CustomUserCreationForm(UserCreationForm):
     profile = forms.ModelChoiceField(queryset=AuthorProfile.objects, required=False, label=_("Profile"))
+    polls = forms.ModelChoiceField(queryset=Question.objects, required=False, label=_("Question"))
+    choice = forms.ModelChoiceField(queryset=Choice.objects, required=False, label=_("Choice"))
