2181ce9c94bd3e7f73b46cc51fbad0e4e5b4b930
LAPTOP-D5RLTO2Q\movma <gabifaltin@seznam.cz>
2022-01-25 18:29:55 +0100

    edit form

--- pages/forms.py
+++ pages/forms.py
@@ -1,7 +1,25 @@
 from django import forms
+from django.forms import TextInput, EmailInput
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px',
+                'placeholder': 'Name'
+                }),
+            'email': EmailInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 300px;',
+                'placeholder': 'Email'
+                }),
+            'message': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px',
+                'placeholder': 'Message'
+            })
+        }
